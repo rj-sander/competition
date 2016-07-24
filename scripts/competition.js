43d11c9633f859d4958bbca27c5f9cf181d576cc
fivefootfood <claire@cjnutrition.co.uk>
2016-07-24 16:39:54 +0100

    playing with things
    we did some cool stuff

--- scripts/competition.js
+++ scripts/competition.js
@@ -10,3 +10,15 @@ $(document).ready(function () {
 	});
 });
 
+function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});
