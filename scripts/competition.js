76cd84c92cd12aebe1647f9ea8f593bceec085cb
krboyce3393 <krboyce3393@hotmail.com>
2016-07-26 10:26:46 +0100

    Added styling and commented all css
    Centred and rounded about image, added lines (hr tags) between work
    content, arranged/added comments to the css

--- scripts/competition.js
+++ scripts/competition.js
@@ -14,8 +14,7 @@ function centerModal() {
     $(this).css('display', 'block');
     var $dialog = $(this).find(".modal-dialog");
     var offset = ($(window).height() - $dialog.height()) / 2;
-    // Center modal vertically in window
-    $dialog.css("margin-top", offset);
+    $dialog.css('margin-top', offset);
 }
 
 $('.modal').on('show.bs.modal', centerModal);
@@ -29,21 +28,25 @@ $(window).scroll(function() {
 
 function sticky_relocate() {
     var window_top = $(window).scrollTop();
-    var div_top = $('#anchor').offset().top;
+    var div_top_work = $('#anchor').offset().top;
     var scrollBottom = $(window).scrollTop() + $(window).height();
-	var enddiv = $('#anchor2').offset().top;
-
-    
-    if ((window_top >= div_top) && (scrollBottom < enddiv)) {
+	var enddiv_work = $('#anchor2').offset().top;
+	    
+    if ((window_top >= div_top_work) && (scrollBottom < enddiv_work)) {
     	$('#work-header').addClass('stick');
+     
     } else {
         $('#work-header').removeClass('stick');
         $('#work-header').height($('#work_content').outerHeight());
-    }
+     }  
+
+	  
 }
 
 $(function() {
     $(window).scroll(sticky_relocate);
     sticky_relocate();
 });
+
+
 
